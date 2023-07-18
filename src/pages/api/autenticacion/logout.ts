20bea384d954bf185974feaddfd8b26ac166985a
Manuel Gonzalez <manuelg9704@gmail.com>
2023-07-18 15:28:18 -0500

    organizando login y logout

---
+++ src/pages/api/autenticacion/logout.ts
@@ -0,0 +1,30 @@
+/* eslint-disable consistent-return */
+import { serialize } from 'cookie';
+import { MY_TOKEN_NAME } from '../../../constantes';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+
+export default function logout(req:NextApiRequest, res:NextApiResponse) {
+  const { token } = req.cookies;
+
+  if (!token) {
+    return res.status(401).json({ message: 'Token No valido' });
+  }
+
+ try {
+    const serialized = serialize(MY_TOKEN_NAME, null, {
+        httpOnly: true,
+        sameSite: 'strict',
+        maxAge: -1,
+        path: '/',
+      });
+
+      res.setHeader('Set-Cookie', serialized);
+      res.status(200).json({ message: 'Cierre de sesión exitoso' });
+    
+ } catch (error) {
+    return res.status(401).json({ error: 'Fallo al cerrar sesión' });
+    
+ }
+
+}
--- src/pages/menu.tsx
+++ src/pages/menu.tsx
@@ -5,34 +5,47 @@ import { useContext, Fragment, useEffect } from 'react';
 import { Button, Menu, message } from 'antd';
 import { EyeTwoTone, UserOutlined } from '@ant-design/icons';
 import { useRouter } from 'next/router';
-import { isEmpty } from 'lodash';
+import { isEmpty, isNull } from 'lodash';
 import AppContext from './api/AppContext';
+import { MAIN_URL } from '../constantes';
+import axios from 'axios';
+import { setUser } from './redux/userSlice';
+import { useDispatch, useSelector } from 'react-redux';
+
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export default function BarraDeNav() {
   const context = useContext(AppContext);
   const router = useRouter();
-
-
-  const handleCerrarSesion = () => {
+  const dispatch = useDispatch();
+  const { nombre, numdoc } = useSelector((state) => state.user);
   
-    context.setMostrarLogin(true);
-    context.llenarForm = [];
-    context.setLlenarForm([]);
+  const handleCerrarSesion = async () => {
+    
+    const response = await axios.post(`${MAIN_URL}/api/autenticacion/logout`)
+    console.log("🚀 ~ response:", response)
+    dispatch(setUser({
+      nombre: '',
+      numdoc: '',
+    }))
+   
     router.push('/auth/login');
   };
 
   return (
     <div>
       <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['']}>
-
-        { context.mostrarLogin && (
+         { !numdoc ?  ( 
         <Menu.Item key="login" icon={<UserOutlined />}>
           <Link className="link" href="/auth/login">
             Login
           </Link>
         </Menu.Item>
-        )}
+        ) :(   <Menu.Item key="logout" >
+        <Button icon={<UserOutlined />} className="link" onClick={() => handleCerrarSesion()}>
+          Logout
+        </Button>
+      </Menu.Item>) } 
         <Menu.Item key="predios" icon={<EyeTwoTone />}>
           <Link className="link" href="/predios">Predios</Link>
         </Menu.Item>
