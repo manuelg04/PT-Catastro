f5a0a389c0e213ed64701592c79fd7e0a3e3b451
Manuel Gonzalez <manuelg9704@gmail.com>
2023-07-18 15:33:07 -0500

    borrando imports que no se usan

--- src/pages/menu.tsx
+++ src/pages/menu.tsx
@@ -1,12 +1,9 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable react-hooks/rules-of-hooks */
 import Link from 'next/link';
-import { useContext, Fragment, useEffect } from 'react';
-import { Button, Menu, message } from 'antd';
+import { Button, Menu } from 'antd';
 import { EyeTwoTone, UserOutlined } from '@ant-design/icons';
 import { useRouter } from 'next/router';
-import { isEmpty, isNull } from 'lodash';
-import AppContext from './api/AppContext';
 import { MAIN_URL } from '../constantes';
 import axios from 'axios';
 import { setUser } from './redux/userSlice';
@@ -15,10 +12,9 @@ import { useDispatch, useSelector } from 'react-redux';
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export default function BarraDeNav() {
-  const context = useContext(AppContext);
   const router = useRouter();
   const dispatch = useDispatch();
-  const { nombre, numdoc } = useSelector((state) => state.user);
+  const { numdoc } = useSelector((state) => state.user);
   
   const handleCerrarSesion = async () => {
     
@@ -33,41 +29,51 @@ export default function BarraDeNav() {
   };
 
   return (
-    <div>
-      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['']}>
-         { !numdoc ?  ( 
-        <Menu.Item key="login" icon={<UserOutlined />}>
-          <Link className="link" href="/auth/login">
-            Login
+    <>
+      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={[""]}>
+        {!numdoc ? (
+          <Menu.Item key="login" icon={<UserOutlined />}>
+            <Link className="link" href="/auth/login">
+              Login
+            </Link>
+          </Menu.Item>
+        ) : (
+          <Menu.Item key="logout">
+            <Button
+              icon={<UserOutlined />}
+              className="link"
+              onClick={() => handleCerrarSesion()}
+            >
+              Logout
+            </Button>
+          </Menu.Item>
+        )}
+        <Menu.Item key="predios" icon={<EyeTwoTone />}>
+          <Link className="link" href="/predios">
+            Predios
           </Link>
         </Menu.Item>
-        ) :(   <Menu.Item key="logout" >
-        <Button icon={<UserOutlined />} className="link" onClick={() => handleCerrarSesion()}>
-          Logout
-        </Button>
-      </Menu.Item>) } 
-        <Menu.Item key="predios" icon={<EyeTwoTone />}>
-          <Link className="link" href="/predios">Predios</Link>
-        </Menu.Item>
         <Menu.Item key="terrenos" icon={<EyeTwoTone />}>
-          <Link className="link" href="/terreno">Terrenos</Link>
+          <Link className="link" href="/terreno">
+            Terrenos
+          </Link>
         </Menu.Item>
         <Menu.Item key="propietarios" icon={<EyeTwoTone />}>
-          <Link className="link" href="/propietarios">Propietarios</Link>
+          <Link className="link" href="/propietarios">
+            Propietarios
+          </Link>
         </Menu.Item>
         <Menu.Item key="construcciones" icon={<EyeTwoTone />}>
-          <Link className="link" href="/construcciones">Construcciones</Link>
+          <Link className="link" href="/construcciones">
+            Construcciones
+          </Link>
         </Menu.Item>
         <Menu.Item key="perfil" icon={<EyeTwoTone />}>
-          <Link className="link" href="/perfil">Perfil</Link>
+          <Link className="link" href="/perfil">
+            Perfil
+          </Link>
         </Menu.Item>
-        {context.mostrarLogin || (
-        <Menu.Item key="cerrarSesion" icon={<EyeTwoTone />}>
-          <Link onClick={handleCerrarSesion} href="/auth/login"> Cerrar Sesion</Link>
-        </Menu.Item>
-        )}
       </Menu>
-    </div>
-
+    </>
   );
 }
